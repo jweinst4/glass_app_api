d9ab9584b8eab385c43ebe7db2ac4fd03838ab9a
jonathan weinstein <jonathanweinstein@jonathans-MBP.home>
2019-09-26 21:30:26 -0400

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins 'localhost:3000'
   
-      resource '*',
+      resource 'localhost:3000',
         headers: :any,
         methods: [:get, :post, :patch, :put, :update, :delete, :options, :head]
     end
